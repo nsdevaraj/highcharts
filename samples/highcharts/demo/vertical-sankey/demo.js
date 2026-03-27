56bbb50643bf4d00f465956c9754631606ca5bf6
PaulDalek <pawel.dalek@blacklabel.pl>
2026-03-27 16:35:17 +0100

    Vertical sankey demo further alignments.

--- samples/highcharts/demo/vertical-sankey/demo.js
+++ samples/highcharts/demo/vertical-sankey/demo.js
@@ -99,7 +99,7 @@ Highcharts.chart('container', {
         }, {
             id: 'Other power (non-steam)',
             column: 4,
-            offsetHorizontal: '900%',
+            offsetHorizontal: '630%',
             color: 'rgba(108, 221, 202, 0.8)'
         }, {
             id: 'To direct water services',
@@ -128,7 +128,7 @@ Highcharts.chart('container', {
             }
         }, {
             id: 'Electric water-related applications',
-            offsetHorizontal: '-330%',
+            offsetHorizontal: '-250%',
             color: 'rgba(255, 141, 100, 0.8)',
             dataLabels: {
                 rotation: -90,
@@ -138,18 +138,18 @@ Highcharts.chart('container', {
             }
         }, {
             id: 'Electric water heating',
-            offsetHorizontal: '-1600%',
+            offsetHorizontal: '-1100%',
             color: 'rgba(250, 183, 118, 0.8)'
         }, {
             id: 'Other',
-            offsetHorizontal: '-700%',
+            offsetHorizontal: '-480%',
             color: 'rgba(149, 155, 236, 0.8)',
             dataLabels: {
                 rotation: -90
             }
         }, {
             id: 'Direct water services',
-            offsetHorizontal: '-680%',
+            offsetHorizontal: '-560%',
             color: 'rgba(76, 175, 254, 0.8)',
             dataLabels: {
                 style: {
@@ -159,11 +159,12 @@ Highcharts.chart('container', {
         }, {
             id: 'Indirect steam use',
             column: 7,
+            offsetHorizontal: '30%',
             color: 'rgba(181, 238, 228, 0.8)'
         }, {
             id: 'Direct steam use',
             column: 7,
-            offsetHorizontal: '200%',
+            offsetHorizontal: '480%',
             color: 'rgba(219, 239, 255, 0.8)',
             dataLabels: {
                 rotation: -90,
